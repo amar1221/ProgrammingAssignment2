a2e6d06be33f87feb3315670704c30b9f859f093
amar1221 <71065603+amar1221@users.noreply.github.com>
2020-09-24 13:20:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
        setsolve = setsolve,
        getsolve = getsolve)
 }
-##
+## For inversed matrix
 ## Same here, changed "mean" to "solve" and "m" to "s"
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
